7152a772426efea6839c99d71558f48a79d27041
Pritpal Singh <prit.17may@ymail.com>
2014-05-15 11:03:08 +0530

    Create backuper.js

---
+++ src/mysql-backuper/js/backuper.js
@@ -0,0 +1,8 @@
+(function($) {
+    $.fn.enable = function() {
+        this.attr('disabled', false);
+    };
+    $.fn.disable = function() {
+        this.attr('disabled', true);
+    };
+}(jQuery));
